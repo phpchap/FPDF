93c758f64b1fee3e98adbe40c01721bbac94b0a8
Your Name <you@example.com>
2017-04-05 10:57:58 +0100

    added calibri to core fonts

--- src/fpdf/FPDF.php
+++ src/fpdf/FPDF.php
@@ -116,7 +116,7 @@ function __construct($orientation='P', $unit='mm', $size='A4')
 	else
 		$this->fontpath = '';
 	// Core fonts
-	$this->CoreFonts = array('courier', 'helvetica', 'times', 'symbol', 'zapfdingbats');
+	$this->CoreFonts = array('courier', 'helvetica', 'times', 'symbol', 'zapfdingbats', 'calibri');
 	// Scale factor
 	if($unit=='pt')
 		$this->k = 1;
